34f474e0cb5eae64f02d43c5bbb46eef30f92e3f
rahi-rahitya <rahityap7@gmail.com>
2022-11-30 16:51:47 +0530

    about props

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/*import ClassComponent from './ClassComponent'*/
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <section>
+    <PropsExample name="sowmya" age="21"/>
+    <PropsExample name="siva" age="50"/>
+       {/*<p>hello</p>
+       <h1>how are you</h1>
+       <ClassComponent/>
+       <img src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg"width="50%"alt="nature"/>*/}
+    </section>
+    )
+}
+export default App
---
+++ src/ClassComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+class ClassComponent extends React.Component{
+  render(){
+	return(
+		<div>
+		this is class Component
+		</div>
+		)
+	}
+}
+export default ClassComponent
